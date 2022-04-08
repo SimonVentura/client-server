477304033891d102b797e4dd7357604d95817fca
SimonVentura <vsimone474@gmail.com>
2022-04-08 14:05:32 +0200

    client server

--- src/main/java/com/mycompany/gestionesoket/Client.java
+++ src/main/java/com/mycompany/gestionesoket/Client.java
@@ -17,12 +17,33 @@ public class Client {
     
     
     public static void main(String[] args) {
+        int port = 2000;
+        Socket socket=null;
         try {
-            //1 richiesta di connesione al server
-            Socket socket = new Socket("127.0.0.1",2000);
-        } catch (IOException ex) {
+            //1 richiesta di connessione al server
+            socket=new Socket("127.0.0.1",port);
+            System.out.println("client connesso con "+ socket.getRemoteSocketAddress());
+            System.out.println("posta del clinet"+ socket.getLocalPort());
+        } 
+                    
+          catch(ConnectExeption ex)
+                  {
+                    System.err.print("server non disponibile");
+                  }
+          
+        
+        catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
+        finally
+        {
+            try {
+                socket.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
     }
 }
     
--- src/main/java/com/mycompany/gestionesoket/Server.java
+++ src/main/java/com/mycompany/gestionesoket/Server.java
@@ -18,17 +18,39 @@ import java.util.logging.Logger;
 public class Server {
 
     public static void main(String[] args) {
+
+           int port = 2000;
+           int time=5000;
+           
+           Socket socket= null;
         try {
+
             //1 server in ascolto in una porta
-            ServerSocket serverSocket=new ServerSocket(2000);
+            ServerSocket serverSocket=new ServerSocket(port);
             System.out.println("server avviato correttamente");
             //2 server in attesa della connessione
-            Socket socket=serverSocket.accept();
-            System.out.println("è stata realizzata la connesione con il client");
-            
+            socket =serverSocket.accept();
+            System.out.println("è stata realizzata la connesione con il client"+ socket.getRemoteSocketAddress());
+
         } catch (IOException ex) {
             System.err.println("errore nella creazione del server e nell'avvio del servizio");
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
+                    //3 scambio dati
+        //4 chiusura socket
+       finally
+        {
+            try
+            {
+                if(socket!=null) 
+                    socket.close();
+            } 
+            catch (IOException ex) 
+            {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
+    
+    
